0ee4a3c377e088e74db2fc35be8ed8f833cb07ff
sxpphic <sphhhsh@gmail.com>
2023-03-02 15:08:14 +0000

    tirando o namespace

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <cctype>
-using	namespace std;
 
 void  print_upper(char *str)
 {
 	int   i = 0;   
 
  	while (str[i])
-    cout << (char) toupper(str[i++]);
+    std::cout << (char) toupper(str[i++]);
 }
 
 int main (int argc, char *argv[])
 {
 	if (argc == 1) {
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl; 
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; 
 	return (0);
 	}
-	*argv++;
+	argv++;
 	while (*argv)
 		print_upper(*argv++);
 	std::cout << std::endl;
