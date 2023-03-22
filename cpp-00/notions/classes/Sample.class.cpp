19dd8fc17386462fc87e66b4a9d71aa2e8b4eae3
vipereir <zzvtrx@gmail.com>
2023-03-22 11:24:28 -0300

    constructor

---
+++ cpp-00/notions/classes/Sample.class.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sample.class.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 11:15:42 by vipereir          #+#    #+#             */
+/*   Updated: 2023/03/22 11:19:03 by vipereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Sample.class.hpp"
+
+Sample::Sample(void){
+	std::cout << "Constructor called" << std::endl;
+	return ;
+}
+
+Sample::~Sample(void){
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
