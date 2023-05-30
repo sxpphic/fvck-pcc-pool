ebb505cf693a04fb7883bb9daeb8c146844a0beb
sooph <sphhhsh@gmail.com>
2023-05-30 15:46:35 -0300

    ex02

---
+++ cpp-01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 15:18:12 by vipereir          #+#    #+#             */
+/*   Updated: 2023/05/30 15:30:13 by vipereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string stringDEV = "D̵̡̢̠̬̝̥͙̘͙̝͓̂Ḛ̶̫͔̎͑̈̈́V̴̢̲̘̗̤͉͒̀͌̒͆́̅̐͐́͋̃̚͝Ḯ̷͙́L̵̩̺̬̉̓̈́̽͐";
+	std::string *stringPTR = &stringDEV;
+	std::string &stringREF = stringDEV;
+
+	std::cout << "stringDEV addr: " << &stringDEV << std::endl;
+	std::cout << "stringPTR addr: " << stringPTR << std::endl;
+	std::cout << "stringREF addr: " << &stringREF << std::endl;
+
+	std::cout << "stringDEV value: " << stringDEV << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+	
+	return (0);
+}
