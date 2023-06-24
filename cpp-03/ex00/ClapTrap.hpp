0304bef48289b10b9c3050ac053bee627fb3af07
sxpph <sphhhsh@gmail.com>
2023-06-24 12:39:32 -0300

    mod

--- cpp-03/ex00/ClapTrap.hpp
+++ cpp-03/ex00/ClapTrap.hpp
@@ -6,12 +6,12 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 13:27:51 by vipereir          #+#    #+#             */
-/*   Updated: 2023/06/23 18:13:13 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/06/24 10:35:38 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_H
-# define CLAPTRAP_H
+#ifndef CLAP_TRAP_H
+# define CLAP_TRAP_H
 # include <iostream>
 
 class	ClapTrap {
