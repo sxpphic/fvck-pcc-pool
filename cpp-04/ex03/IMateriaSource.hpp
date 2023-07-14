c62ef42f5b1f3c11629d7a046506769b861aa0be
sxpph <sphhhsh@gmail.com>
2023-07-14 12:38:10 -0300

    fix: typo in ifndef

--- cpp-04/ex03/IMateriaSource.hpp
+++ cpp-04/ex03/IMateriaSource.hpp
@@ -6,11 +6,11 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 12:13:06 by vipereir          #+#    #+#             */
-/*   Updated: 2023/06/30 16:42:23 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/07/14 12:37:30 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef I_MATEREAL_SOURCE_H
+#ifndef I_MATERIAL_SOURCE_H
 # define I_MATERIAL_SOURCE_H
 # include "AMateria.hpp"
 
