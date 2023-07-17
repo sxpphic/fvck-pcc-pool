148e761a5dcf06f88a51ae0ca4b1a1e6a009f480
sxpph <sphhhsh@gmail.com>
2023-07-17 09:29:18 -0300

    sobe sobe

---
+++ cpp-05/ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 11:47:58 by vipereir          #+#    #+#             */
+/*   Updated: 2023/07/16 11:54:04 by vipereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+# define BUREAUCRAT_H
+
+# include <string>
+# include <iostream>
+
+class Bureaucrat {
+
+	public:
+
+	private:
+		const 
+	
+};
+
+#endif
