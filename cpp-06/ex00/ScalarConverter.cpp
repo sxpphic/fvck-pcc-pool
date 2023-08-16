c7f927a7c1bd878a8b9d8bf7f59919a49f830f12
sxpphic <72941725+sxpphic@users.noreply.github.com>
2023-08-16 19:04:38 +0000

    add: convertions

--- cpp-06/ex00/ScalarConverter.cpp
+++ cpp-06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 16:35:09 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/15 16:35:11 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/16 19:03:31 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,12 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 int int_type(const std::string& str_num) {
  	std::istringstream buffer(str_num);
 
-	int val;
+	//double nuu = atof(static_cast<std::string>("nanf").c_str());
+
+//	std::cout << "test: " << nuu << std::endl;
+
+	char val; // = atoi(str_num.c_str());
+	
 	buffer >> val;
 	// tem q terminar
 	//std::cout << "char: " << "'" << ((isprint(val)) ? static_cast<char>(val) : "Non displayable") << "'" << std::endl;
@@ -47,14 +52,12 @@ int int_type(const std::string& str_num) {
 	std::cout << "float: " << static_cast<float>(val) << "f" << std::endl;
 	std::cout << "double: " << static_cast<double>(val) << std::endl;
 
-
-
 	return (val);
 }
 
 int check_is_valid(const std::string& str) { // falta tratar nan e inf
 	short dot_counter = 0;
-	if (str.length() == 1) // to achando q devo passar os char com ''
+	if (str.length() == 1) // to achando q devo passar os char com '' // verificar se é char ou numero
 		return (TYPE_CHAR);
 
 	size_t start = (str[0] == '-' || str[0] == '+') ? 1 : 0;
@@ -83,6 +86,9 @@ void ScalarConverter::convert(const std::string& num) {
 		return ;
 	switch (type) {
 		case TYPE_INT:
+			int_type(num);
+			break;
+		case TYPE_CHAR:
 			int_type(num);
 			break;
 		default:
--- cpp-06/ex00/ScalarConverter.hpp
+++ cpp-06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 16:35:13 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/15 16:35:24 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/16 18:51:53 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <sstream>
 # include <iomanip>
 # include <cctype>
+# include <stdlib.h>
 
 # define TYPE_CHAR 1
 # define TYPE_INT 2
