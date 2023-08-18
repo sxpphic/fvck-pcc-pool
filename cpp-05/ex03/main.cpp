f766eaa4e928d193bb0ce299d93ebb132a9cbe41
sxpphic <72941725+sxpphic@users.noreply.github.com>
2023-08-18 13:25:21 +0000

    add testes

--- cpp-05/ex03/main.cpp
+++ cpp-05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 14:30:57 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/18 13:14:16 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/18 13:24:51 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@
 
 int main(void) {
 	try {
-			Bureaucrat zoid("zoid", 1);
+			Bureaucrat random("random", 1);
 			Intern someRandomIntern;
 			AForm* rrf;
-			rrf = someRandomIntern.makeForm("presidential pardon", "random target");
+			rrf = someRandomIntern.makeForm("presidential pardon", "random_target");
 
 			if (rrf) {
 				std::cout << *rrf << std::endl;
+				random.signForm(*rrf);
+				random.executeForm(*rrf);
 				delete rrf;
 			}
 	}
