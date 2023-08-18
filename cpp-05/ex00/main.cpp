1a46db9bcfa303bc8ec4453b6e3b19d4fe4422b1
sxpphic <72941725+sxpphic@users.noreply.github.com>
2023-08-18 12:05:39 +0000

    ex00

--- cpp-05/ex00/main.cpp
+++ cpp-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 14:30:57 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/02 18:36:50 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/18 12:04:39 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 int main(void) {
 	try
 	{
-		Bureaucrat *a = new Bureaucrat("lavinsky", 156);
+		Bureaucrat a("lavinsky", 1);
 		//a.increment();
 		//a.decrement();
 		//a.decrement();
 		//a.decrement();
 		//a.decrement();
-		std::cout << *a << std::endl;
-		delete a;
+		std::cout << a << std::endl;
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
