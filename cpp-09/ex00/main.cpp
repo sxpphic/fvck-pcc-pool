eebffbb37b80bad93a5b8b5beefdca182abca58b
sxpphickat <sphhhsh@gmail.com>
2023-08-21 11:43:53 -0300

    add cpp09 files

---
+++ cpp-09/ex00/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 11:15:16 by vipereir          #+#    #+#             */
+/*   Updated: 2023/08/21 11:22:12 by vipereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		std::cout << "wrong imput!!" << std::endl;
+		return (0);
+	}
+	// checar se o data.csv existe.
+	
+	return (0);
+}
