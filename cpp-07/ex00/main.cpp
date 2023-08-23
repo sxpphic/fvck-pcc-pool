4ea2d850f8cd2c141c51f31f454f4f9a4b587be8
sxpphickat <sphhhsh@gmail.com>
2023-08-23 16:04:55 -0300

    cpp07

--- cpp-07/ex00/main.cpp
+++ cpp-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:15:25 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/09 18:18:28 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/23 14:33:59 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main( void ) {
 	int a = 2;
 	int b = 3;
 
-	::swap( a, b );
+	::swap<int>( a, b );
 	
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
--- cpp-07/ex02/Array.hpp
+++ cpp-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 19:22:40 by vipereir          #+#    #+#             */
-/*   Updated: 2023/08/09 20:08:35 by vipereir         ###   ########.fr       */
+/*   Updated: 2023/08/23 16:02:29 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,52 @@
 # define Array_H
 # include <iostream>
 # include <string>
-# include "Array.tpp"
+
+template <typename T>
+class Array {
+	
+	public:
+ 		Array() {
+			_size = 0;
+			_dynamic_arr = new T[0];
+		};
+		Array(unsigned int size) {
+			_size = size;
+			_dynamic_arr = new T[size];
+		};
+		Array(const Array& other) {
+			_size = other._size;
+			_dynamic_arr = new T[_size];
+			for (unsigned int i = 0; i < _size; i++) {
+				_dynamic_arr[i] = other._dynamic_arr[i];
+			}
+		};
+		~Array() {
+			delete[] _dynamic_arr;
+		};
+		Array& operator=(const Array& other) {
+			_size = other._size;
+			delete[] _dynamic_arr;
+			_dynamic_arr = new T[_size];
+			for (unsigned int i = 0; i < _size; i++) {
+				_dynamic_arr[i] = other._dynamic_arr[i];
+			}
+			return (*this);
+		};
+		unsigned int size(void) {
+			return (_size);
+		}
+		T& operator[](unsigned int index) {
+			if (index >= _size)
+				throw std::runtime_error("index out of bounds");
+			return (_dynamic_arr[index]);
+		}
+
+	private:
+		unsigned int	_size;
+		T*				_dynamic_arr;
+		
+};
 
 #endif
 
